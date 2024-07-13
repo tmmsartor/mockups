5ce3d451303005ddaf006a0fb86f9c97ba1d1836
Joris Gillis <joris@yacoda.com>
2024-07-13 11:53:34 +0200

    wor on madnlp

--- madnlp/src/madnlp.cpp
+++ madnlp/src/madnlp.cpp
@@ -1,10 +1,10 @@
-#include "MadnlpCInterface.h"
+#include "madnlp_c.h"
 
-MADNLP_SYMBOL_EXPORT void madnlp_c_startup(int, char**) { }
 MADNLP_SYMBOL_EXPORT struct MadnlpCSolver* madnlp_c_create(struct MadnlpCInterface* nlp_interface) { return 0; }
 MADNLP_SYMBOL_EXPORT void madnlp_c_init(struct MadnlpCSolver* s) { }
-MADNLP_SYMBOL_EXPORT void madnlp_c_update_cco_indexes(struct MadnlpCSolver* s) { }
-MADNLP_SYMBOL_EXPORT madnlp_int madnlp_c_solve(struct MadnlpCSolver*) { return 0; }
+MADNLP_SYMBOL_EXPORT madnlp_int madnlp_c_solve(struct MadnlpCSolver* s) { return 0; }
+MADNLP_SYMBOL_EXPORT struct MadnlpCNumericIn* madnlp_c_input(struct MadnlpCSolver* s) { return 0; }
+MADNLP_SYMBOL_EXPORT struct MadnlpCNumericOut* madnlp_c_output(struct MadnlpCSolver* s) { return 0; }
 
 /* -1 for not found, 0 for double, 1 for int, 2 for bool, 3 for string */
 MADNLP_SYMBOL_EXPORT int madnlp_c_option_type(const char* name) { return 0; }
@@ -13,7 +13,5 @@ MADNLP_SYMBOL_EXPORT int madnlp_c_set_option_bool(struct MadnlpCSolver* s, const
 MADNLP_SYMBOL_EXPORT int madnlp_c_set_option_int(struct MadnlpCSolver* s, const char* name, int val) { return 0; }
 MADNLP_SYMBOL_EXPORT int madnlp_c_set_option_string(struct MadnlpCSolver* s, const char* name, const char* val) { return 0; }
 
-MADNLP_SYMBOL_EXPORT const struct MadnlpCDims* madnlp_c_get_dims(struct MadnlpCSolver* s) { return 0; }
 MADNLP_SYMBOL_EXPORT const struct MadnlpCStats* madnlp_c_get_stats(struct MadnlpCSolver* s) { return 0; }
 MADNLP_SYMBOL_EXPORT void madnlp_c_destroy(struct MadnlpCSolver*) { }
-MADNLP_SYMBOL_EXPORT void madnlp_c_shutdown(void) { }
