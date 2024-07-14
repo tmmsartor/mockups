1db6f3395002ee582446a4ef70a2d0f0b8cf0aef
Joris Gillis <joris@yacoda.com>
2024-07-14 22:47:29 +0200

    fix header

--- madnlp/include/madnlp_c.h
+++ madnlp/include/madnlp_c.h
@@ -23,7 +23,7 @@ extern "C" {
 #endif
 
 
-#define madnlp_int int
+#define madnlp_int long long int
 #define madnlp_real double
 
 // structs
@@ -35,15 +35,11 @@ struct MadnlpCInterface;
 typedef struct MadnlpCSolver MadnlpCSolver;
 
 // function pointer types
-typedef madnlp_int (*MadnlpCEvalObj)(const double*, double *, void*);
-typedef madnlp_int (*MadnlpCEvalConstr)(const double*, double *, void*);
-typedef madnlp_int (*MadnlpCEvalObjGrad)(const double*, double*, void*);
-typedef madnlp_int (*MadnlpCEvalConstrJac)(const double*, double*, void*);
-typedef madnlp_int (*MadnlpCEvalLagHess)(double, const double*, const double*, double*, void*);
-
-struct MadnlpCStats {
-  madnlp_int iter;
-};
+typedef int (*MadnlpCEvalObj)(const double*, double *, void*);
+typedef int (*MadnlpCEvalConstr)(const double*, double *, void*);
+typedef int (*MadnlpCEvalObjGrad)(const double*, double*, void*);
+typedef int (*MadnlpCEvalConstrJac)(const double*, double*, void*);
+typedef int (*MadnlpCEvalLagHess)(double, const double*, const double*, double*, void*);
 
 struct MadnlpCInterface {
   MadnlpCEvalObj eval_obj;
@@ -85,21 +81,30 @@ struct MadnlpCNumericOut {
   const double* mul;
   const double* mul_L;
   const double* mul_U;
+  const double* primal_feas;
+  const double* dual_feas;
+};
+
+struct MadnlpCStats {
+  const madnlp_int* iter;
 };
 
 MADNLP_SYMBOL_EXPORT struct MadnlpCSolver* madnlp_c_create(struct MadnlpCInterface* nlp_interface);
+
 MADNLP_SYMBOL_EXPORT const struct MadnlpCNumericIn* madnlp_c_input(struct MadnlpCSolver*);
-MADNLP_SYMBOL_EXPORT const struct MadnlpCNumericOut* madnlp_c_output(struct MadnlpCSolver*);
-MADNLP_SYMBOL_EXPORT madnlp_int madnlp_c_solve(struct MadnlpCSolver*);
 
 /* -1 for not found, 0 for double, 1 for int, 2 for bool, 3 for string */
 MADNLP_SYMBOL_EXPORT int madnlp_c_option_type(const char* name);
 MADNLP_SYMBOL_EXPORT int madnlp_c_set_option_double(struct MadnlpCSolver* s, const char* name, double val);
-MADNLP_SYMBOL_EXPORT int madnlp_c_set_option_bool(struct MadnlpCSolver* s, const char* name, int val);
-MADNLP_SYMBOL_EXPORT int madnlp_c_set_option_int(struct MadnlpCSolver* s, const char* name, int val);
+MADNLP_SYMBOL_EXPORT int madnlp_c_set_option_bool(struct MadnlpCSolver* s, const char* name, madnlp_int val);
+MADNLP_SYMBOL_EXPORT int madnlp_c_set_option_int(struct MadnlpCSolver* s, const char* name, madnlp_int val);
 MADNLP_SYMBOL_EXPORT int madnlp_c_set_option_string(struct MadnlpCSolver* s, const char* name, const char* val);
 
+MADNLP_SYMBOL_EXPORT int madnlp_c_solve(struct MadnlpCSolver*);
+
+MADNLP_SYMBOL_EXPORT const struct MadnlpCNumericOut* madnlp_c_output(struct MadnlpCSolver*);
 MADNLP_SYMBOL_EXPORT const struct MadnlpCStats* madnlp_c_get_stats(struct MadnlpCSolver* s);
+
 MADNLP_SYMBOL_EXPORT void madnlp_c_destroy(struct MadnlpCSolver*);
 
 #ifdef __cplusplus
