c2b2c274d887beedef12188b305695df6a1990d7
Joris Gillis <joris.gillis42@gmail.com>
2024-07-28 09:24:48 +0200

    fixup! Matlab add engine

--- matlab/include/engine.h
+++ matlab/include/engine.h
@@ -1,19 +1,18 @@
 #ifndef engine_H
 #define engine_H
-#include "eng_Export.h"
 
 extern "C"
 {
   typedef struct mxArray_tag mxArray;
   typedef struct engine Engine;
 
-  eng_Export int engEvalString(Engine *, const char *);
-  eng_Export Engine* engOpenSingleUse(const char *, void *, int *);
-  eng_Export Engine *engOpen(const char *);
-  eng_Export int engClose(Engine *ep);
-  eng_Export mxArray *engGetVariable(Engine *, const char *);
-  eng_Export int engPutVariable(Engine *, const char *, const mxArray *);
-  eng_Export int engOutputBuffer(Engine *, char *, int);
+  int engEvalString(Engine *, const char *);
+  Engine* engOpenSingleUse(const char *, void *, int *);
+  Engine *engOpen(const char *);
+  int engClose(Engine *ep);
+  mxArray *engGetVariable(Engine *, const char *);
+  int engPutVariable(Engine *, const char *, const mxArray *);
+  int engOutputBuffer(Engine *, char *, int);
 
 }
 
